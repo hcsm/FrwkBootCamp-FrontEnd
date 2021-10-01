e038f1b694a5222e51c95a7ac3b3a3ce6759afd5
Victor <victorafonso@frwk.com.br>
2021-10-01 12:01:50 -0300

    chore - Estrutura cadastro

---
+++ src/features/cadastro/Cadastro.tsx
@@ -0,0 +1,29 @@
+// @flow
+import * as React from 'react';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+`
+
+
+type Props = {
+
+};
+export const Cadastro = (props: Props) => {
+  return (
+    <Container className="">
+      <Wrapper>
+
+      </Wrapper>
+    </Container>
+  );
+};
