b544e7acdf1a5d98a64e06c9b9091712f272903e
BrenoES <breno.esilva@outlook.com>
2021-10-04 09:38:11 -0300

    ci: configurações das URLs de dev e produção do build dos MFE.

--- root-config/src/frameworker-root-config.ts
+++ root-config/src/frameworker-root-config.ts
@@ -1,11 +1,30 @@
 import { registerApplication, start } from "single-spa";
+interface Application {
+  name: string;
+  package: string;
+  activeWhen: string;
+  exact: string;
+}
 
-registerApplication({
-  name: "@frameworker/react-single",
-  app: () => System.import("@frameworker/react-single"),
-  activeWhen: ["/"],
-});
+interface ApplicationResponseData {
+  applications: Application[];
+}
 
-start({
-  urlRerouteOnly: true,
-});
+fetch("https://run.mocky.io/v3/b0ae8a06-fed8-4bce-94ce-8d76b33e86fc")
+  .then((response) => response.json())
+  .then((data: ApplicationResponseData) =>
+    data.applications.forEach((application) => {
+      registerApplication({
+        name: application.name,
+        app: () => System.import(application.package),
+        activeWhen: application.exact
+          ? (location) => location.pathname === application.activeWhen
+          : [application.activeWhen],
+      });
+    })
+  )
+  .finally(() =>
+    start({
+      urlRerouteOnly: true,
+    })
+  );
