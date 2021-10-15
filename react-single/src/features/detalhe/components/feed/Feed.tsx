ce096556190d29a04567703a97133d36fdb3c033
Everton Cruz <evertoncruz@frwk.com.br>
2021-10-15 14:00:16 -0300

    Create feed component

---
+++ react-single/src/features/detalhe/components/feed/Feed.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {
+  WrapperFeed,
+} from './styles'
+
+type Props = {}
+
+export const Feed = (props: Props) => {
+  return (
+    <WrapperFeed>
+      <p>Componente Feed</p>
+    </WrapperFeed>
+  );
+}
