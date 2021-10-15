91f3f735d66302c0cf4f340548ed76e0d776cf93
Everton Cruz <evertoncruz@frwk.com.br>
2021-10-15 14:03:23 -0300

    create detail feature route

--- react-single/src/Routes.tsx
+++ react-single/src/Routes.tsx
@@ -1,14 +1,17 @@
 // @flow
 import * as React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
-import { Cadastro } from './features/cadastro/Cadastro'
 import { Home } from './features/Home'
+import { Cadastro } from './features/cadastro/Cadastro'
+import { Detalhe } from './features/detalhe/Detalhe'
+
 type Props = {}
 const Routes = (props: Props) => {
   return (
     <Switch>
       <Route exact path="/home" component={Home} />
       <Route path="/cadastro" component={Cadastro} />
+      <Route path="/detalhe" component={Detalhe} />
       <Redirect from="*" to="/" />
     </Switch>
   )
