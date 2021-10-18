95ec62bdc8dc1fb3cc21271196fc62380bd72fc4
Everton Cruz <evertoncruz@frwk.com.br>
2021-10-18 18:18:50 -0300

    aside left and section style adjustment

--- react-single/src/features/detalhe/styles.ts
+++ react-single/src/features/detalhe/styles.ts
@@ -14,10 +14,10 @@ export const WrapperContent = styled.div`
 `
 
 export const WrapperAside = styled.aside`
-  flex: 2;
+  flex: 3;
   background-color: #090B20;
 `
 
 export const WrapperSection = styled.aside`
-  flex: 10;
+  flex: 9;
 `
