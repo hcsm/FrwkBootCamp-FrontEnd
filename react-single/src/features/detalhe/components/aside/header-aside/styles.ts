035e3ea3faa0386564c3e480847cd6a54a6fd30a
Everton Cruz <evertoncruz@frwk.com.br>
2021-10-19 10:30:18 -0300

    create headeraside component

---
+++ react-single/src/features/detalhe/components/aside/header-aside/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 314px;
+`
+
+export const Image = styled.img`
+height: 141px;
+width: 141px;
+border-radius: 141px;
+background-color: #FFF;
+`
+
+export const WrapperTitle = styled.div`
+  padding: 28px 4px 0px 4px;
+`
+
+export const Title = styled.p`
+  text-align: center;
+  font-family: Roboto;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 23px;
+  letter-spacing: 0em;
+  color: #FFF;
+`
+
+export const Button = styled.button`
+  padding: 0;
+  margin-left: 14px;
+  background-color: transparent;
+  border none;
+`
