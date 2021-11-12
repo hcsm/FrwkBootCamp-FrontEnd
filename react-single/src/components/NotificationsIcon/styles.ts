c50b193f7900bc7f598cfacf55ddff2b1661def8
Pedro Vilaça <pedrovilacav@gmail.com>
2021-11-12 14:40:35 -0300

    Adicionado icone para fechar notificações

---
+++ react-single/src/components/NotificationsIcon/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const CloseIconWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  color: #fff;
+  background-color: #213054;
+  padding: 10px 20px 0 0;
+  /* padding-left: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px; */
+`
