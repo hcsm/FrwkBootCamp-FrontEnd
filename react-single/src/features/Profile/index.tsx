f82e5f511480988fa172dca1a959c2149f8c38b9
erivelton <eriveltonborges@frwk.com.br>
2021-11-16 20:31:08 +0000

    Implementando o botão voltar e e editar foto

--- react-single/src/features/Profile/index.tsx
+++ react-single/src/features/Profile/index.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import { Header } from '../../components/Header'
 import Detail from './components/Detail'
 import Technology from './components/Technology'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useHistory } from 'react-router-dom'
 import { useGetProfessionalsQuery } from '../../services/users'
+import IconCircle from '../../components/IconCircle'
 
-import { WrapperProfile, WrapperBanner, WrapperContent } from './styles'
+import { WrapperProfile, WrapperBanner, WrapperContent, BackButton, Button } from './styles'
 
 type Props = {}
 
@@ -13,15 +14,36 @@ const Profile = (props: Props) => {
   let location = useLocation();
   const onlyRead =  (location.state==="read" || location.state===undefined) 
   const { data, isError } = useGetProfessionalsQuery()
+  const history = useHistory()
 
   const professional =  data?.filter(
       user => String(user.professionalId) === location.search.substring(1)
     )[0]
    
+  function handleBack() {
+    history.push('/goBack')
+  }
+
   return (
     <WrapperProfile>
       <Header />
+     
       <WrapperBanner />
+      <div className='d-flex justify-content-end'>
+        <Button>
+          <IconCircle
+            color="white"
+            borderColor="#213054"
+            icon='Edit'
+            backgroundColor="#213054"
+            hasShadow
+          />
+        </Button>
+      </div>
+
+      <BackButton onClick={handleBack}>
+        <IconCircle color="white" icon="KeyboardBackspace" />
+      </BackButton>  
 
       <WrapperContent>
         <Detail props = {professional ? professional : {}}
--- react-single/src/features/Profile/styles.ts
+++ react-single/src/features/Profile/styles.ts
@@ -15,3 +15,26 @@ export const WrapperContent = styled.div`
   display: flex;
   flex-wrap: wrap;
 `
+
+export const BackButton = styled.button`
+  border-radius: 100px;
+  background: #0B0C22;
+  width: 46px;
+  height: 46px;
+  position: absolute;
+  margin-top: -150px;
+  margin-left: 8px;
+`
+
+export const Button = styled.button`
+  padding: 0;
+  margin-right: 5px;
+  background-color: transparent;
+  border: none;
+  top: 0px;
+  display: flex;
+  width: 31px;
+  height: 31px;
+  margin-left: 5px;
+  margin-top: -40px;
+`
